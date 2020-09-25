1966328dd7a1b9be822dd3c14878807b0438498d
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-09-26 01:50:36 +0700

    update database

--- Backup Database/gachawallet.sql
+++ Backup Database/gachawallet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Sep 2020 pada 10.07
+-- Waktu pembuatan: 25 Sep 2020 pada 20.44
 -- Versi server: 10.4.10-MariaDB
 -- Versi PHP: 7.3.12
 
@@ -31,45 +31,73 @@ SET time_zone = "+00:00";
 CREATE TABLE `histories` (
   `id` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
-  `action` varchar(256) NOT NULL,
+  `idOther` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `idOther` int(11) NOT NULL,
-  `note` varchar(256) NOT NULL
+  `note` varchar(256) NOT NULL,
+  `transaction` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `histories`
+--
+
+INSERT INTO `histories` (`id`, `idUser`, `idOther`, `amount`, `time`, `note`, `transaction`) VALUES
+(1, 1, 2, 1000, '2020-09-25 18:08:10', 'nyoba transfer', 'Transfer'),
+(3, 2, 1, 500000, '2020-09-25 18:13:36', 'coba transfer', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `phonenumber`
+-- Struktur dari tabel `phonenumbers`
 --
 
-CREATE TABLE `phonenumber` (
+CREATE TABLE `phonenumbers` (
   `id` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   `phoneNumber` varchar(256) NOT NULL,
   `priority` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `phonenumbers`
+--
+
+INSERT INTO `phonenumbers` (`id`, `idUser`, `phoneNumber`, `priority`) VALUES
+(1, 1, '082322059666', '1'),
+(2, 1, '082345394535', '2'),
+(3, 1, '23423423534', '2'),
+(4, 2, '4834953845', '2'),
+(5, 2, '081xxxxxxxxx', '1');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Struktur dari tabel `users`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(256) NOT NULL,
   `email` varchar(64) NOT NULL,
   `password` varchar(256) NOT NULL,
   `roleId` int(8) NOT NULL,
-  `pin` varchar(8) NOT NULL,
+  `pin` varchar(128) NOT NULL,
   `image` varchar(256) NOT NULL,
   `firstName` varchar(256) NOT NULL,
   `lastName` varchar(256) NOT NULL,
   `infoUser` varchar(256) NOT NULL,
-  `Saldo` int(11) NOT NULL
+  `saldo` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `roleId`, `pin`, `image`, `firstName`, `lastName`, `infoUser`, `saldo`) VALUES
+(1, 'cafeinai', 'cafeinai311@gmail.com', '$2a$10$1CPqulqV.aLV3L9tseIa9u9U4UIUsKJ6yFce4X9vqDUDsi/z25/9i', 1, '692025', 'http://localhost:4000/uploads/1600942807476-Cafeinai.jpg', 'Ervin', 'Nurhediyanto', 'Fullstack Developer', '6000'),
+(2, 'erubin', 'cafeinai689@gmail.com', '$2a$10$VXREqL6r.1lS2YJ7pXB4t.3jSOsgrNSs/eONLweLH4EfMVYco57Oy', 2, '', '', '', '', '', ''),
+(4, 'erubin', 'cafeinai321@gmail.com', '$2a$10$VXREqL6r.1lS2YJ7pXB4t.3jSOsgrNSs/eONLweLH4EfMVYco57Oy', 2, '', '', '', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -79,18 +107,20 @@ CREATE TABLE `user` (
 -- Indeks untuk tabel `histories`
 --
 ALTER TABLE `histories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUser` (`idUser`);
 
 --
--- Indeks untuk tabel `phonenumber`
+-- Indeks untuk tabel `phonenumbers`
 --
-ALTER TABLE `phonenumber`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `phonenumbers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUser` (`idUser`);
 
 --
--- Indeks untuk tabel `user`
+-- Indeks untuk tabel `users`
 --
-ALTER TABLE `user`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -101,19 +131,29 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `histories`
 --
 ALTER TABLE `histories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `phonenumber`
+-- AUTO_INCREMENT untuk tabel `phonenumbers`
 --
-ALTER TABLE `phonenumber`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `phonenumbers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT untuk tabel `users`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `phonenumbers`
+--
+ALTER TABLE `phonenumbers`
+  ADD CONSTRAINT `phonenumbers_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
